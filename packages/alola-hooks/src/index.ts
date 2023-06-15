fbfca709ebf679d257673a1dfeb51b3e78072d76
nguyenhainam24082000 <nghainam24082000>
2023-06-15 10:50:57 +0700

    feat(packages): add use-isomorphic-effect

--- packages/alola-hooks/src/index.ts
+++ packages/alola-hooks/src/index.ts
@@ -1,2 +1,3 @@
 export { useClickOutside } from './use-click-outside/use-click-outside'
+export { useIsomorphicEffect } from './use-isomorphic-effect/use-isomorphic-effect'
 export { useOrientation } from './use-orientation/use-orientation'
