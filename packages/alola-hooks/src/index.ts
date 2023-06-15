6e0ad89a9557feb9e159834a477ae69bd3a91246
nguyenhainam24082000 <nghainam24082000>
2023-06-15 10:53:18 +0700

    feat(packages): add use-eye-dropper

--- packages/alola-hooks/src/index.ts
+++ packages/alola-hooks/src/index.ts
@@ -1,3 +1,4 @@
 export { useClickOutside } from './use-click-outside/use-click-outside'
+export { useEyeDropper } from './use-eye-dropper/use-eye-dropper'
 export { useIsomorphicEffect } from './use-isomorphic-effect/use-isomorphic-effect'
 export { useOrientation } from './use-orientation/use-orientation'
